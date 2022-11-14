41ed35cff495dbab2bd319ad108b50fa776684b4
Valeriy Kozlov <Valera.31.07.9.9@gmail.com>
2022-11-14 12:35:12 +0200

    add components and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
